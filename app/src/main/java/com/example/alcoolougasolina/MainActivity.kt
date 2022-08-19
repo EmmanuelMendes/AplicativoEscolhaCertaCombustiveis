7af4107f34d3e678428a471004f515c4460157e5
Emmanuel Mendes <36781486+EmmanuelMendes@users.noreply.github.com>
2022-08-19 00:20:26 -0300

    atualizacao aplicativo

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun btCalcular(view: View){
+
+        val precoAlcool = findViewById(R.id.preco_alcool) as EditText
+        val resultadoAlcool = precoAlcool.text.toString()
+
+        val precoGasolina = findViewById(R.id.preco_gasolina) as EditText
+        val resultadoGasolina = precoGasolina.text.toString()
+
+        val valorAlcool = resultadoAlcool.toDouble()
+        val valorGasolina = resultadoGasolina.toDouble()
+
+        val texto = findViewById(R.id.resultado) as TextView
+
+        val calculoCombustivel = valorAlcool / valorGasolina
+
+
+
+        if(calculoCombustivel >= 0.7){
+            texto.setText("Melhor utilizar Gasolina")
+        }else {
+            texto.setText("Melhor utilizar Alcool")
+        }
+
+
+
+    }
+
 }
